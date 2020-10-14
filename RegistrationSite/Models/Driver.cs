aaba1d8746c67e8c1a1ba73dec444e48742b7e6b
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2020-10-14 14:35:46 -0400

    -Added classes for registration

---
+++ RegistrationSite/Models/Driver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationSite.Models
+{
+    public class Driver
+    {
+    }
+}
